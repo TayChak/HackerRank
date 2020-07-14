d5d2771ef8e01836ac494a1ef3c8ff32d0c1fe91
TheElite <47625834+TayChak@users.noreply.github.com>
2020-07-14 09:54:53 +0200

    Update Weather Observation Station 6.sql

--- Practice/SQL/Weather Observation Station 6.sql
+++ Practice/SQL/Weather Observation Station 6.sql
@@ -1,2 +1,2 @@
 SELECT DISTINCT CITY FROM STATION WHERE CITY RLIKE '^[aeiouAEIOU]' //Query the list of CITY names starting with vowels (a, e, i, o, u) from STATION
-SELECT DISTINCT CITY FROM STATION WHERE CITY RLIKE  "[aeiouAEIOU]$"; // Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION
+SELECT DISTINCT CITY FROM STATION WHERE CITY RLIKE  '[aeiouAEIOU]$'; // Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION
