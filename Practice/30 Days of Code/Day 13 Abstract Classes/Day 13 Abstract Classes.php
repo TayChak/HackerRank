00aee04395979b5c8e42ebe4179385e315472430
TheElite <47625834+TayChak@users.noreply.github.com>
2020-07-23 11:17:22 +0200

    Add files via upload

---
+++ Practice/30 Days of Code/Day 13 Abstract Classes/Day 13 Abstract Classes.php
@@ -0,0 +1,35 @@
+<?php
+abstract class Book
+{
+
+    protected $title;
+    protected  $author;
+
+     function __construct($t,$a){
+        $this->title=$t;
+        $this->author=$a;
+    }
+    abstract protected function display();
+}
+
+//Write MyBook class
+class MyBook extends Book
+{
+    protected $price;
+    function __construct($title, $author, $price) {
+        parent::__construct(trim($title), trim($author));
+        $this->price = $price;
+    }
+    function display() {
+        echo 'Title: ' . $this->title . "\n";
+        echo 'Author: ' . $this->author . "\n";
+        echo 'Price: ' . $this->price . "\n";
+    }
+}
+$title=fgets(STDIN);
+$author=fgets(STDIN);
+$price=intval(fgets(STDIN));
+$novel=new MyBook($title,$author,$price);
+$novel->display();
+
+?>
