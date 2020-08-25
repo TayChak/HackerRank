115c2990222eb90461d8cd9f14e5f5e499c112ca
TheElite <47625834+TayChak@users.noreply.github.com>
2020-08-25 12:39:08 +0200

    Add files via upload

---
+++ Practice/30 Days of Code/Day 20 Sorting/Day 20 Sorting.php
@@ -0,0 +1,25 @@
+<?php
+
+$handle = fopen ("php://stdin", "r");
+fscanf($handle, "%d",$n);
+$a_temp = fgets($handle);
+$a = explode(" ",$a_temp);
+$a = array_map('intval', $a);
+// Write Your Code Here
+$numberOfSwaps = 0;
+for($i=0; $i<$n; $i++) {
+    for($j=0; $j<$n-1; $j++) {
+        if ($a[$j] > $a[$j+1]) {
+            $temp = $a[$j];
+            $a[$j] = $a[$j+1];
+            $a[$j+1] = $temp;
+            $numberOfSwaps++;
+        }
+    }
+    if ($numberOfSwaps == 0) break;
+}
+echo "Array is sorted in ".$numberOfSwaps." swaps.\n";
+echo "First Element: ".$a[0]."\n";
+echo "Last Element: ".$a[$n-1]."\n";
+
+?>
